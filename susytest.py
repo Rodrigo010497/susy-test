597be04513f40593381d2370a6af88851b2d8392
Rodrigo010497 <didigo010497@gmail.com>
2025-04-18 14:40:14 +0100

    functionality to save scores to db

--- susytest.py
+++ susytest.py
@@ -1,5 +1,6 @@
 import pymysql
 import os
+from datetime import datetime
 #house class
 class House:
     #Attributes
@@ -13,37 +14,47 @@ class House:
         self.potential_savings_on_improvements = F
         self.effective_savings_on_improvements = G
         self.co2_saved = H
-        self.savings_on_project_improvements_prediction_error = { month: (F[month] - G[month]) for month in months }
-        self.potential_score = self.__calculate_potential_score()
-        self.effective_score = self.__calculate_effective_score()
+        self.savings_on_project_improvements_prediction_error = { date: (F[date] - G[date]) for date in self.months }
+        self.potential_scores_per_month = {date: self.calculate_potential_score(date) for date in self.months}
+        self.effective_score_per_month = {date: self.calculate_effective_score(date) for date in self.months}
 
 
     #calculate potential score
-    def __calculate_potential_score(self):
-        return ( (self.potential_energy_generation + self.potential_savings_on_improvements) - self.energy_usage)
+    def calculate_potential_score(self, date):
+        score = ( (self.potential_energy_generation[date] + self.potential_savings_on_improvements[date]) - self.energy_usage[date] )
+        print(date,"potential score",score)
+        return score
     
     #calculate effective score
-    def __calculate_effective_score(self):
-        return ( (self.effective_energy_generation + self.effective_energy_generation) - self.energy_usage)
-    @property
-    #get potential score
-    def potential_score(self):
-        self.potential_score = 0
-        return self.potential_score
-    @property
-    #get effective score
-    def effective_score(self): 
-        return self.effective_score
+    def calculate_effective_score(self, date):
+        score = ( (self.effective_energy_generation[date] + self.effective_energy_generation[date]) - self.energy_usage[date])
+        print(date,"effective score",score)
+        return score
+    
+    # @property
+    # #get potential score
+    # def potential_score(self):
+    #     return self.potential_score
+
+    # @property
+    # #get effective score
+    # def effective_score(self): 
+    #     return self.effective_score
     
 
     #save potential score to database
-    def save_potential_score(self, cursor, conn):
-        cursor.execute(f"INSERT INTO houses (potential_score) VALUES {self.potential_score}")
+    def save_potential_score(self, conn, cursor, date):
+        date = datetime.strptime(date, '%Y-%m-%d').date()
+        cursor.execute(f"UPDATE houses SET potential_score = {self.potential_scores_per_month[date]} WHERE date = {date};")
         conn.commit()  # Commit the transaction
 
     #save effective score to database
-    def save_effective_score(self): 
+    def save_effective_score(self):
+        
         return 0 
+
+
+
 
     #Connect to database 
 def conenct_to_db():
@@ -71,8 +82,8 @@ def close_db_connection(conn, cursor):
 def get_houses_data_from_db(cursor, first_month_date, last_month_date):
     cursor.execute(f"SELECT * FROM houses WHERE date BETWEEN '{first_month_date}' AND '{last_month_date}'")  #Adjust table name as needed
     rows = cursor.fetchall()
-    variables_names = ["A","B","C","D","E","F","G"]
-    data_per_month = { name: { str(row["date"]) : row[name] for row in rows } for name in variables_names }
+    variables_names = ["A","B","C","D","E","F","G","H"]
+    data_per_month = { name: { row["date"] : row[name] for row in rows } for name in variables_names }
 
     # Print the data from the table
     for row in rows:
@@ -90,11 +101,6 @@ def get_houses_data_from_db(cursor, first_month_date, last_month_date):
 
     # print("Data inserted successfully!")
 
-    
-
-
-
-
 os.environ["DB_HOST"] = "127.0.0.1"
 os.environ["DB_USER"] = "root"
 os.environ["DB_PASSWORD"] = "123"
@@ -103,6 +109,7 @@ os.environ["DB_PORT"] = "3306"
 conn, cursor = conenct_to_db()
 data = get_houses_data_from_db(cursor, "2023-04-01", "2023-06-30")
 house = House(**data)
+house.save_potential_score(conn, cursor, "2023-04-01")
 
 
 
